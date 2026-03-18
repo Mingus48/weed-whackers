e6ea757595b41845368ac924012cbc197b79af11
Mingus48 <prahlad.rao55@gmail.com>
2026-03-17 20:56:36 -0400

    Running animations

--- scenes/Player.cs
+++ scenes/Player.cs
@@ -4,6 +4,8 @@ using System;
 public partial class Player : CharacterBody2D
 {
 	private int maxSpeed = 300;
+	[Export]
+	private AnimatedSprite2D anim;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready(){
@@ -13,9 +15,21 @@ public partial class Player : CharacterBody2D
 	public override void _PhysicsProcess(double delta){
 		Vector2 axisPowers = Input.GetVector("left", "right", "up", "down");
 		if(axisPowers == Vector2.Zero){
-			//Friction
+			//Idle anim
+			anim.Play("idle");
 		}else{
-			//Acceleration
+			//Move anim
+			if(axisPowers.X != 0){
+				if(axisPowers.X > 0){
+					anim.Play("runRight");
+				}else{
+					anim.Play("runLeft");
+				}
+			}else if(axisPowers.Y > 0){
+				anim.Play("runDown");
+			}else{
+				anim.Play("runUp");
+			}
 		}
 		Velocity = axisPowers * maxSpeed;
 		//GD.Print(Velocity);
